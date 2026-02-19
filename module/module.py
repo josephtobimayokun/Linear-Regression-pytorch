e59565d00533def32ad720587ac3c3e1a414dee1
Joseph Tobi Mayokun <josephtobimayokun@gmail.com>
2026-02-19 06:24:51 +0100

    Create module.py

---
+++ module/module.py
@@ -0,0 +1,47 @@
+import torch
+
+from torch import nn
+
+
+
+class Module(nn.Module):
+
+    
+
+    def __init__(self):
+
+        super().__init__()
+
+
+
+    def loss(self, y_hat, y):
+
+        raise NotImplementedError
+
+
+
+    def forward(self, X):
+
+        raise NotImplementedError
+
+
+
+    def trainer_step(self, batch):
+
+        l = self.loss(self(*batch[:-1]), batch[-1])
+
+        return l
+
+
+
+    def validation_step(self, batch):
+
+        l = self.loss(self(*batch[:-1]), batch[-1])
+
+        return l
+
+
+
+    def configure_optimizer(self):
+
+        raise NotImplementedError
